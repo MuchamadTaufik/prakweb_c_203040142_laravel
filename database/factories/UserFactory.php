37e2335a8a5af36945aede453245131ae2add17f
MuchamadTaufik <muhamadtaufikm10@gmail.com>
2022-12-21 13:31:17 +0700

    ppp

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,7 +2,6 @@
 
 namespace Database\Factories;
 
-
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
@@ -17,7 +16,6 @@ class UserFactory extends Factory
     {
         return [
             'name' => $this->faker->name(),
-            'username' => $this->faker()->unique()->userName(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
